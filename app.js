66550f9a4cd668d11e4bfb1cd6db3449834849db
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-08-30 12:57:38 -0700

    Added a search bar and implemented the Giphy API to display all trending gifs. Also refractored code.

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
+// REQUIRE HTTP MODULE
+var http = require('http');
+// INITIALIZE THE GIPHY-API LIBRARY
+var giphy = require('giphy-api')();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'https://media.giphy.com/media/k5w4LN7G5mK2I/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello Squirrel!');
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  });
 });
 
 app.listen(3000, function () {
